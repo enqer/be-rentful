8727ab41ce9aae2c551487d1858c465fe5ef2e30
enqer <pawel.kam4@gmail.com>
2024-07-31 18:35:39 +0200

    build: application

--- src/Rentful.Api/Program.cs
+++ src/Rentful.Api/Program.cs
@@ -1,4 +1,5 @@
 using Rentful.Api;
+using Rentful.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
-
+builder.Services.AddApplication();
 builder.ConfigureOptions();
 
 

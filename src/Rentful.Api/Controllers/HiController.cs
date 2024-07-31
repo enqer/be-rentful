829d93795f16e3b55d106d933ac55dcd434b35b1
enqer <pawel.kam4@gmail.com>
2024-07-31 18:28:36 +0200

    build: api

---
+++ src/Rentful.Api/Controllers/HiController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rentful.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HiController(ILogger<HiController> logger) : ControllerBase
+    {
+   
+        private readonly ILogger<HiController> _logger = logger;
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("Hello, World!");
+        }
+    }
+}
---
+++ src/Rentful.Api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace Rentful.Api
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureOptions(this WebApplicationBuilder webApplicationBuilder)
+        {
+        }
+
+        private static WebApplicationBuilder ConfigureOption<T>(this WebApplicationBuilder webApplicationBuilder) where T : class
+        {
+            webApplicationBuilder.Services.Configure<T>(webApplicationBuilder.Configuration.GetSection(typeof(T).Name));
+            return webApplicationBuilder;
+        }
+    }
+}

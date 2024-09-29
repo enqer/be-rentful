c64ed8d822755600b2489a697622d60ded45ff20
enqer <pawel.kam4@gmail.com>
2024-09-29 17:16:01 +0200

    feat: address entity

---
+++ src/Rentful.Domain/Entities/Address.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rentful.Domain.Entities
+{
+    public class Address
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("postal_code")]
+        public string PostalCode { get; set; } = string.Empty;
+        [Column("building_number")]
+        public string BuildingNumber { get; set; } = string.Empty;
+        [Column("street")]
+        public string Street { get; set; } = string.Empty;
+        [Column("city")]
+        public string City { get; set; } = string.Empty;
+        [Column("country")]
+        public string Country { get; set; } = string.Empty;
+    }
+}

37ec670e30050784093be9f78bb86527840dda5e
enqer <pawel.kam4@gmail.com>
2024-10-28 20:52:24 +0100

    feat: new apartment endpoint

---
+++ src/Rentful.Api/Controllers/ApartmentController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Rentful.Application.UseCases.Commands.RegisterUser;
+
+namespace Rentful.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ApartmentController(IMediator mediator) : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewApartment([FromBody] RegisterUserUseCase.Command command)
+        {
+            await mediator.Send(command);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ src/Rentful.Application/UseCases/Commands/NewApartment/Dtos/Coordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace Rentful.Application.UseCases.Commands.NewApartment.Dtos
+{
+    public class Coordinate
+    {
+        public double Lng { get; set; }
+        public double Lat { get; set; }
+    }
+}

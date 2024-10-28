ba89db272518231e36d32296f7079821b7ad026a
enqer <pawel.kam4@gmail.com>
2024-10-28 20:50:46 +0100

    feat: location entity

---
+++ src/Rentful.Domain/Entities/Location.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rentful.Domain.Entities
+{
+    [Table("locations", Schema = "public")]
+    public class Location
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("latitude")]
+        public decimal Latitude { get; set; }
+
+        [Column("longitude")]
+        public decimal Longitude { get; set; }
+
+        [Column("place")]
+        public string? Place { get; set; } = string.Empty;
+
+    }
+}
---
+++ src/Rentful.Infrastructure/Persistence/Configurations/LocationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rentful.Domain.Entities;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder
+            .Property(u => u.Latitude)
+            .IsRequired();
+        builder
+            .Property(u => u.Longitude)
+            .IsRequired();
+
+    }
+}

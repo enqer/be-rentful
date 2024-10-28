f81180905e83a191c547751931a837c213ba2937
enqer <pawel.kam4@gmail.com>
2024-10-28 20:51:36 +0100

    feat: announcement entity

---
+++ src/Rentful.Domain/Entities/Announcement.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rentful.Domain.Entities
+{
+    [Table("announcements", Schema = "public")]
+    public class Announcement
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("title")]
+        public string Title { get; set; } = string.Empty;
+        [Column("description")]
+        public string Description { get; set; } = string.Empty;
+        [Column("date_added")]
+        public string DateAdded { get; set; } = string.Empty;
+
+        [Column("apartment_id")]
+        public int? ApartmentId { get; set; }
+        public Apartment Apartment { get; set; } = new Apartment();
+    }
+}
---
+++ src/Rentful.Infrastructure/Persistence/Configurations/AnnouncementConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rentful.Domain.Entities;
+
+public class AnnouncementConfiguration : IEntityTypeConfiguration<Announcement>
+{
+    public void Configure(EntityTypeBuilder<Announcement> builder)
+    {
+        builder
+            .HasKey(a => a.Id);
+        builder
+           .HasOne(u => u.Apartment)
+           .WithOne()
+           .HasForeignKey<Announcement>(x => x.ApartmentId)
+           .OnDelete(DeleteBehavior.Cascade);
+        builder
+            .Property(u => u.Title)
+            .IsRequired();
+        builder
+            .Property(u => u.Description)
+            .IsRequired();
+        builder
+            .Property(u => u.DateAdded)
+            .IsRequired();
+
+    }
+}

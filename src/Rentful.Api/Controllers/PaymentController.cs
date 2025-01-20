beb95bb9fe773125dde917ca70f391fd84a1c5e3
enqer <pawel.kam4@gmail.com>
2025-01-20 22:45:31 +0100

    feat: add new payment

---
+++ src/Rentful.Api/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Rentful.Application.UseCases.Commands.AddPayment;
+
+namespace Rentful.Api.Controllers
+{
+    public class PaymentController(IMediator mediator) : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddReport([FromBody] AddPaymentUseCase.Command command)
+        {
+            await mediator.Send(command);
+            return Ok();
+        }
+    }
+}
---
+++ src/Rentful.Application/UseCases/Commands/AddPayment/AddPaymentUseCase.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Rentful.Application.Common.Interfaces;
+using Rentful.Domain.Entities;
+using Rentful.Domain.Exceptions;
+using System.Net;
+
+namespace Rentful.Application.UseCases.Commands.AddPayment
+{
+    public static class AddPaymentUseCase
+    {
+        public record Command(int AgreementId) : IRequest;
+
+        internal class Handler(IRepository repository) : IRequestHandler<Command>
+        {
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var leaseAgreement = repository.LeaseAgreements.FirstOrDefault(x => x.Id == request.AgreementId)
+                     ?? throw new HttpResponseException(HttpStatusCode.NotFound, "Błąd dodania płatności", "Nie można odnaleźć umowy");
+
+                var newPayment = new Payment
+                {
+                    Date = DateTime.UtcNow.ToString(),
+                    Amount = leaseAgreement.Price + leaseAgreement.Rent ?? 0,
+                };
+                leaseAgreement.Balance += newPayment.Amount;
+                leaseAgreement.Payments.Add(newPayment);
+                repository.SaveChanges();
+
+                await Task.CompletedTask;
+            }
+        }
+    }
+}
